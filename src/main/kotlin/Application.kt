4143f6820fba02bf261b146906b9ca34a4217034
Aaron Smith <asmitty92@gmail.com>
2020-12-06 18:08:26 -0600

    Initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,24 @@
+package com.ohgnarly
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/") {
+            defaultResource("index.html", "clientApp")
+            resources("clientApp")
+        }
+    }
+}
+
